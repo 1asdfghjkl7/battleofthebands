e7cc3e00436a0cba943ac975d4a49e34bd5d3daa
Jordan <jordanwilliams288@gmail.com>
2018-05-23 13:55:14 -0500

    completed challenges for battleofthebands

---
+++ main.js
@@ -0,0 +1,13 @@
+let bandNumber = 0;
+
+const takeNumber = function (bandName) {
+
+    bandNumber++
+    console.log(`${bandNumber} ${bandName}`);
+    
+
+}
+
+takeNumber("Galactic Scum");
+takeNumber("Underdogs");
+
